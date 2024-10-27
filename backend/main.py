0a0c12847a9539bb68699b781cb83ebfa5d7d92e
Akhil562! <akhilraghavakalal@gmail.com>
2024-10-27 15:48:09 -0500

    RR-19 Manage Products feature developement

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
 
 from fastapi import FastAPI 
+from fastapi.staticfiles import StaticFiles
+
 from fastapi.middleware.cors import CORSMiddleware 
 origins=['http://localhost:3000']
 
 
 
 
-from src.routes import auth_routes
+from src.routes import auth_routes,product_routes
 
 app = FastAPI()
 
@@ -20,9 +22,11 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
-
-# Register the routes
+app.mount("/upload_images", StaticFiles(directory="uploaded_images"), name="upload_images")
 app.include_router(auth_routes.router)
+app.include_router(product_routes.router)
+
+
 
 if __name__ == "__main__":
     import uvicorn
